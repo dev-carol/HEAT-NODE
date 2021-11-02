3e940de2b11d15f2e5d86aecd6b6963207bb2790
carol santos <contato.caroline1999@gmail.com>
2021-11-02 02:02:03 -0300

    feat: token for user

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -5,10 +5,15 @@ class AuthenticateUserController{
     async handle(request: Request, response: Response){
 
       const {code} = request.body;
-      const service = new AuthenticateUserService();
-      const result = await service.execute(code);
 
-      return response.json(result);
+      const service = new AuthenticateUserService();
+      try {
+        const result = await service.execute(code);
+        return response.json(result);
+        
+      } catch (error) {
+        return response.json(error.message);
+      }
         
     }
 }
