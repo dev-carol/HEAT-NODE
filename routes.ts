ad36ebd45a49d2071dd7794dd42408f362bb8083
carol santos <contato.caroline1999@gmail.com>
2021-11-02 21:48:31 -0300

    feat: verifcation token invalid

--- routes.ts
+++ routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./src/controllers/AuthenticateUserController";
+import { CreateMessageController } from "./src/controllers/CreateMessageController";
+import { ensureAuthenticated } from "./src/middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle)
+
+router.post("/messages",
+ ensureAuthenticated,
+new CreateMessageController().handle
+);
 
 export {router};
